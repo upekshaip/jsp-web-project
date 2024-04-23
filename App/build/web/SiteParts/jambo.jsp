19634acce8020a2100ef3e9acff81da116d57d92
upekshaip <upekshaip@gmail.com>
2024-04-23 22:31:27 +0530

    resolve the config issues

--- App/build/web/SiteParts/jambo.jsp
+++ App/build/web/SiteParts/jambo.jsp
@@ -6,7 +6,7 @@
 
 <section class="container defalt-container-style"
          style="position:relative; background: url('<%=url%>'); background-position: center; background-repeat: no-repeat; background-size: cover; padding: 0;">
-    <div style="width:100%; height:100%; background-color: rgba(0, 0, 0, 0.4);">
+    <div style="width:100%; background-color: rgba(0, 0, 0, 0.4);">
         <div class="text-white rounded" style="padding: 100px 50px">
             <b><h1 style="color:white; text-transform: uppercase; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
                     <%=topic%>
--- App/src/java/Test.java
+++ App/src/java/Test.java
@@ -21,22 +21,9 @@ public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
+        
 
         PrintWriter out = response.getWriter();
-        DB db = new DB();
-
-        HttpSession session = request.getSession(false);
-        HashMap user = (HashMap) session.getAttribute("user");
-        String user_id = (String) user.get("uid");
-        ResultSet rs = db.getOrders(user_id);
-        try {
-            while (rs.next()) {
-                out.println(rs.getString("username"));
-            }
-            out.print("asdasd");
-        } catch (SQLException ex) {
-            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        out.println("hello");
     }
 }
